2375c6c97da33bf254ff80c79eba6fb4c319fd14
IamThiago-IT <thiagodossantos315@gmail.com>
2020-12-23 08:52:06 -0300

    App

---
+++ Projetos/Dia_19/js/app.js
@@ -0,0 +1,44 @@
+const cityForm = document.querySelector('[data-js="change-location"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+const cityCard = document.querySelector('[data-js="city-card"]')
+let timeImg = document.querySelector('[data-js="time"]')
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
+
+const showCityCard = () => {
+  if (cityCard.classList.contains('d-none')) {
+    cityCard.classList.remove('d-none')
+  }
+}
+
+const showCityWeatherInfo = async cityName => {
+  const [{ key, LocalizedName }] = await getCityData(cityName)
+  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(key)
+  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+
+
+
+  timeImg.src = IsDayTime ? './src/day.svg' : './src/night.svg'
+  timeIconContainer.innerHTML = timeIcon
+  cityNameContainer.textContent = localizedName
+  cityWeatherContainer.textContent = weatherText
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
+
+cityForm.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const inputValue = event.target.city.value
+
+  showCityCard()
+  showCityWeatherInfo(inputValue)
+  localStorage.setItem('city', inputValue)
+  cityForm.reset()
+})
+
+if (city) {
+  showCityCard()
+  showCityWeatherInfo(city)
+
+}
